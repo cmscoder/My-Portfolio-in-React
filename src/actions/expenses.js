60f49aff0785e93360aa323c4f57ed42a473367c
Camila Medeiros <camila.medeiros.silva2013@gmail.com>
2021-02-05 16:24:03 +0000

    Setup test database env

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -1,22 +1,44 @@
 import { v4 as uuid } from "uuid";
+import database from "../firebase/firebase";
+// use push
+// attach then callback
+// dispatch action
+// redirect
 
+// components calls action generator
+// action generator returns funcion
+// component dispatches funciont (?)
+// function runs (has the ability to dispatch others actions and do whatever ir wants)
 // ADD_EXPENSE
 
-export const addExpense = ({
-  description = "",
-  note = "",
-  amount = 0,
-  createdAt = 0,
-} = {}) => ({
+export const addExpense = (expense) => ({
   type: "ADD_EXPENSE",
-  expense: {
-    id: uuid(),
-    description,
-    note,
-    amount,
-    createdAt,
-  },
+  expense,
 });
+
+export const startAddExpense = (expenseData = {}) => {
+  return (dispatch) => {
+    const {
+      description = "",
+      note = "",
+      amount = 0,
+      createdAt = 0,
+    } = expenseData;
+    const expense = { description, note, amount, createdAt };
+
+    return database
+      .ref("expenses")
+      .push(expense)
+      .then((ref) => {
+        dispatch(
+          addExpense({
+            id: ref.key,
+            ...expense,
+          })
+        );
+      });
+  };
+};
 
 // REMOVE_EXPENSE
 
--- webpack.common.js
+++ webpack.common.js
@@ -1,13 +1,20 @@
 const path = require("path");
+const webpack = require("webpack");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-  mode: "development",
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
+if (process.env.NODE_ENV === "test") {
+  require("dotenv").config({ path: ".env.test" });
+} else if (process.env.NODE_ENV === "development") {
+  require("dotenv").config({ path: ".env.development" });
+}
+
+module.exports = {
   entry: "./src/app.js",
   output: {
-    path: path.resolve(__dirname, "public", "dist"),
     filename: "bundle.js",
+    path: path.resolve(__dirname, "public", "dist"),
   },
   performance: {
     hints: false,
@@ -40,5 +47,27 @@ module.exports = {
       },
     ],
   },
-  plugins: [new MiniCssExtractPlugin({ filename: "styles.css" })],
+  plugins: [
+    new MiniCssExtractPlugin({ filename: "styles.css" }),
+    new webpack.DefinePlugin({
+      "process.env.FIREBASE_API_KEY": JSON.stringify(
+        process.env.FIREBASE_API_KEY
+      ),
+      "process.env.FIREBASE_AUTH_DOMAIN": JSON.stringify(
+        process.env.FIREBASE_AUTH_DOMAIN
+      ),
+      "process.env.FIREBASE_DATABASE_URL": JSON.stringify(
+        process.env.FIREBASE_DATABASE_URL
+      ),
+      "process.env.FIREBASE_PROJECT_ID": JSON.stringify(
+        process.env.FIREBASE_PROJECT_ID
+      ),
+      "process.env.FIREBASE_STORAGE_BUCKET": JSON.stringify(
+        process.env.FIREBASE_STORAGE_BUCKET
+      ),
+      "process.env.FIREBASE-MESSAGING_SENDER_ID": JSON.stringify(
+        process.env.FIREBASE_MESSAGING_SENDER_ID
+      ),
+    }),
+  ],
 };
